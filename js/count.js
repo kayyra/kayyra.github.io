047b20edeef21de67953888ca023bd99d2d0fbd4
Ana <127655123+kayyra@users.noreply.github.com>
2023-07-18 16:20:44 +0200

    Add files via upload

--- js/count.js
+++ js/count.js
@@ -8,25 +8,25 @@ $finalMessage = document.querySelector('.message');
 const countdownDate = new Date('July 19, 2023 09:00:00').getTime();
 
 let interval = setInterval(function(){
-    //Obtener fecha actual y milisegundos
+   
     const now = new Date().getTime();
 
-    //Obtener las distancias entre ambas fechas
+  
     let distance = countdownDate - now;
 
-    //Calculos a dias-horas-minutos-segundos
+   
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
 
-    //Escribimos resultados
+  
     $days.innerHTML = days;
     $hours.innerHTML = hours;
     $minutes.innerHTML = minutes;
     $seconds.innerHTML = ('0' + seconds).slice(-2);
 
-    //Cuando llegue a 0
+ 
     if(distance < 0){
         clearInterval(interval);
         $finalMessage.style.transform = 'translateY(0)';
